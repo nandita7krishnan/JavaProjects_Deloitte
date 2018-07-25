051ff246fff80170baf7829973b8efc4d5069135
Nandita <nandita7krishnan@gmail.com>
2018-07-25 14:42:40 +0530

    import ke tareekein

---
+++ importsAndAll/src/com/importsandall/MainClass.java
@@ -0,0 +1,18 @@
+package com.importsandall;
+
+import java.util.*;
+import java.sql.Date;
+public class MainClass {
+
+	LinkedList result;
+	Calendar cal;
+	java.util.Date date;
+	
+	public void insertDetails(Date db) {
+		
+	}
+	public MainClass() {
+		// TODO Auto-genera ted constructor stub
+	}
+
+}
