4b77228990de283b0145af1e5131687b3a90e751
Nandita <nandita7krishnan@gmail.com>
2018-07-25 14:40:36 +0530

    setters and getters

---
+++ Encapsulation/src/com/deloitte/what/Employee.java
@@ -0,0 +1,35 @@
+package com.deloitte.what;
+
+
+
+public class Employee {
+	private int deptId;
+	private int empId;
+	private String firstName;
+	private String lastName;
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
